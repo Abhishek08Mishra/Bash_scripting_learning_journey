d343c625b1145385cf279424cb50bb9af417280d
Abhishek08Mishra <abishekmishra195@gmail.com>
2026-01-22 14:17:55 +0545

    learned read command and basic input validation

---
+++ basic/exe1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter your name"
+
+read name
+
+echo "Welcome, $name !"
+
+# The read command is used to take input from the user.
+# This script asks the user to enter their name, stores the input in a variable called "name",
+# and then displays a greeting message in the terminal: "Welcome, <name>!"
