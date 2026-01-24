791a6d01949c8d8edb4ccf4744997c7020086fed
Abhishek08Mishra <abishekmishra195@gmail.com>
2026-01-24 20:41:56 +0545

    fixed typo error

--- conditionals/absolute_value.sh
+++ conditionals/absolute_value.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Exercise 4 — Absolute Value
-# Ask for a number. Print its absolute value (positive version).
-# Focus:`if` and arithmetic
 
 # Taking input from the user
 read -p "Enter your number : " num
-echo 
+echo
+
 # Validate input
 if ! [[ "$num" =~ ^-?[0-9]+$ ]]; then
     echo "Please enter a valid number."
