e4702475c53d00247e0d4591b020ed325c5700e2
Abhishek08Mishra <abishekmishra195@gmail.com>
2026-02-01 13:16:42 +0545

    add features timestamp to overwrite a file

--- loops_automation/for_loop/exe16.sh
+++ loops_automation/for_loop/exe16.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Backup all bash(.sh) file in a single file.
+timestamp=$(date +%Y%m%d_%H%M%S)
 for file in *.sh; do
-	cat "$file" >> backup.txt
+    cat "$file" >> "backup_$timestamp.txt"
 done
-echo "All Backup completed"
+echo "Backup saved as backup_$timestamp.txt"
