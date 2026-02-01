8bdc329f2da4a02c85e5eb0ea8452438aa9fcced
Abhishek08Mishra <abishekmishra195@gmail.com>
2026-02-01 14:36:14 +0545

    Fix bug: showing each created file name that makes terminal mess and add feature to total count at last of the script

--- loops_automation/for_loop/exe10.sh
+++ loops_automation/for_loop/exe10.sh
@@ -5,8 +5,10 @@
 read -p "Enter file extension:" extension
 read -p "Enter no. of files that you want to create :" total_file
 
+count=0
 for i in $( seq 1 $total_file ); do # seq --> generates a sequences of number from 1 to $totalfile.
 	filename="file$i$extension"
+	count=$(( count +1 ))
 	touch "$filename"
-	echo "File created : $filename"
 done
+echo "Total file created : $count"
