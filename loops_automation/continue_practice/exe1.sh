7899107a79d12e29cf12ad8df7eee1270aeb0fcf
Abhishek08Mishra <abishekmishra195@gmail.com>
2026-02-03 21:12:00 +0545

    learned how to use continue, break with case while making system menu

---
+++ loops_automation/continue_practice/exe1.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Task --> Menu System
+# Show a menu repeatedly:
+# 1) Show date
+# 2) Show current user
+# 3) Exit
+# Loop forever until user selects 3, then use break to stop.
+
+#!/bin/bash
+
+while true; do
+	echo
+	echo "	********	"
+	echo "	  MENU 	"
+	echo "	********	"
+	echo
+	echo "1.) Show date"
+	echo "2.) Show current user"
+	echo "3.) Exit"
+	echo
+
+	read -p "Enter your option from the above Menu: " userinput
+	echo
+
+	# Reject non-numeric input (like 5k, abc, etc.)
+	if ! [[ "$userinput" =~ ^[0-9]+$ ]]; then
+		echo "======================================================"
+		echo "Please enter a valid option from the menu (eg; 1,2,3)"
+		echo "======================================================"
+		continue
+	fi
+
+	case $userinput in
+		1)
+			echo "================================================="
+			echo "Current Time : $(date)"
+			echo "================================================="
+			;;
+		2)
+			echo "========================"
+			echo "Current user : $(whoami)"
+			echo "========================"
+			;;
+		3)
+			echo "========"
+			echo " Exited"
+			echo "========"
+			break
+			;;
+		*)
+			echo "======================================================"
+			echo "Please enter a valid option from the menu (eg; 1,2,3)"
+			echo "======================================================"
+			;;
+	esac
+done
