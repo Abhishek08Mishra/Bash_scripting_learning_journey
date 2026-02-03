cbd8f069ddb0ed3a783d81597e73b380e5724cd4
Abhishek08Mishra <abishekmishra195@gmail.com>
2026-02-03 21:37:27 +0545

    error fixed..

--- loops_automation/break_practice/exe1.sh
+++ loops_automation/break_practice/exe1.sh
@@ -3,10 +3,8 @@
 # Task --> Print numbers from 1 to 10, but stop the loop completely when number = 5.
 
 for num in {1..10}; do
-
 	if (( num == 5 )); then
 		break
 	fi
-	
-	echo "$num"
+echo "$num"
 done
