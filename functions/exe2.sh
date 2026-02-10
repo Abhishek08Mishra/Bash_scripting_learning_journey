b277a653c93c9d83cfd7684e3b9ddf3efcc7e72a
Abhishek08Mishra <abishekmishra195@gmail.com>
2026-02-10 22:10:45 +0545

    learn how to handles return in functions

---
+++ functions/exe2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#--------------------------------
+# Task 2: Even or Odd
+# Goal: Use return.
+# Requirements:
+# Function name: is_even
+# Takes one number
+# Returns 0 if even, 1 if odd
+# Use if to print:
+# Even number
+# Odd number
+#-------------------------------
+
+is_even() {
+	num=$(( $1 ))
+	if (( num % 2 == 0 )); then
+		echo "Even Number"
+		return 0
+	else
+		echo "Odd number"
+		return 1
+	fi
+}
+is_even 8
