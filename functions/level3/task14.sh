7a43d2ec8b1f6a52f52cded361b181e8c9840f27
Abhishek08Mishra <abishekmishra195@gmail.com>
2026-02-14 01:13:50 +0545

    built a program that shows file size in human readable format with a limit

---
+++ functions/level3/task14.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Task 14: Function to find files larger than X MB
+
+check_file_size() {
+
+	read -rp "Enter file path : " path
+	
+	# Path validation
+	if ! [[ -e "$path" ]]; then
+		echo "Invalid path selection"
+		return 1
+	fi
+	
+	# list all files >1MB in the directory, show their size in MB, and sort largest first.	
+	local filesize="$(find "$path" -type f -size +50M -exec du -BM {} + | sort -n)"
+	
+	echo "$filesize"
+}
+
+check_file_size
