6443e84472f4a56ea8318557930aaa7eeae8d9fa
Abhishek08Mishra <abishekmishra195@gmail.com>
2026-02-16 20:10:36 +0545

    pull functions out from the loop

--- functions/level1/combined_task.sh
+++ functions/level1/combined_task.sh
@@ -1,7 +1,57 @@
 #!/bin/bash
 
-while true; do
+#===================================================|
+# 1) Function to display host name on the terminal. |
+#===================================================|
+get_hostname() {
+	local hname=$(hostname)
+	echo "Host name : $hname"
+}
+
+
+#================================================================|
+# 2) Function to get current user and display it on the terminal.|
+#================================================================|
+get_currentuser() {
+	local curruser=$(whoami)
+	echo "Current User : $curruser"
+}
+
+
+#===================================|
+# 3) Function to get system uptime. |
+#===================================|
+get_sysuptime() {
+	local syuptme=$(uptime)
+	echo "System Uptime : $syuptme"
+}
+
+
+#===========================================|
+# 4) Function to get current date and time. |
+#===========================================|
+get_date_time() {
+	local currdatime=$(date)
+	echo "Current date and time : $currdatime"
+}
+
+
+#==============================================================|
+# 5) Function to get operating system name and kernel version. |
+#==============================================================|
+get_os_kernel() {
+	local osname=$(uname -o)
+	echo "Operating System name : $osname"
+	echo
+	local kernelversion=$(uname -v)
+	echo "Kernel Version : $kernelversion "
+	echo
+}
+
+
+while true;do
 	clear
+	echo
 	echo "-----------------"
 	echo "System Awareness"
 	echo "-----------------"
@@ -18,46 +68,22 @@ while true; do
 	
 	case "${opt,,}" in
 	1)
-		get_hostname() {
-			local hname=$(hostname)
-			echo "Host name : $hname"
-		}
 		get_hostname
 		;;
 	
 	2)
-		get_currentuser() {
-			local curruser=$(whoami)
-			echo "Current User : $curruser"
-		}
 		get_currentuser
 		;;
 
 	3)
-		get_sysuptime() {
-			local syuptme=$(uptime)
-			echo "System Uptime : $syuptme"
-		}
 		get_sysuptime
 		;;
 	
 	4)
-		get_date_time() {
-			local currdatime=$(date)
-			echo "Current date and time : $currdatime"
-		}
 		get_date_time
 		;;
 	
 	5)
-		get_os_kernel() {
-			local osname=$(uname -o)
-			echo "Operating System name : $osname"
-			echo
-			local kernelversion=$(uname -v)
-			echo "Kernel Version : $kernelversion "
-			echo
-		}
 		get_os_kernel
 		;;
 	
