08e27c70748a7ace42c37138bc0cc64a90fac03b
Abhishek08Mishra <abishekmishra195@gmail.com>
2026-02-28 15:10:48 +0545

    add verbose flag -v and fix typo error

--- arguments_flags/sum_args.sh
+++ arguments_flags/sum_args.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+VERBOSE=false
+
+usage () {
+	echo "Usage : $0 [-v] num1 num2"
+	echo "-v 		Enable verbose output"
+	exit 1
+}
+
+while getopts "v" opt; do
+	case "$opt" in
+		v) VERBOSE=true ;;
+		*) usage ;;
+	esac
+done
+
+shift $(( OPTIND -1 )) 
+
 # Input validation
 if [[ "$#" != 2 ]]; then
 	echo "Error: Exactly 2 arguments are required"
+	usage
 	exit 1
 fi
 
@@ -10,21 +28,21 @@ num1=$1
 num2=$2
 
 #Validate number
-if ! [[ "$num1" =~ ^-?[0-9]+$ ]] || ! [[ "$num1" =~ ^-?[0-9]+$ ]]; then
+if ! [[ "$num1" =~ ^-?[0-9]+$ ]] || ! [[ "$num2" =~ ^-?[0-9]+$ ]]; then
 	echo "Error: Arguments must be integers"
 	exit 1
 fi 
 
 add=$((num1 + num2))
 
-echo "-------------------------------------"
-echo "Script name : $0"
-echo "First number : $1"
-echo "Second number : $2"
-echo "Sum : $add"
-echo "All arguments : $@"
-echo "Number of arguments : $#"
-echo "-------------------------------------"
+# Verbose output
+$VERBOSE && echo "[VERBOSE] Script name: $0"
+$VERBOSE && echo "[VERBOSE] First number: $num1"
+$VERBOSE && echo "[VERBOSE] Second number: $num2"
+$VERBOSE && echo "[VERBOSE] Total arguments: $#"
+$VERBOSE && echo "[VERBOSE] All arguments: $@"
 
-echo "Arguments look good"
+# Main output
+echo "Sum: $add"
+
 exit 0
